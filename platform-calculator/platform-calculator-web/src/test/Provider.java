1e33613f0895c2faba39a65a820ec5d1a5a3b8a9
Lenovo y520 <894687269@qq.com>
2018-02-28 17:52:12 +0800

    登陆 注册功能

--- platform-calculator/platform-calculator-web/src/test/Provider.java
+++ platform-calculator/platform-calculator-web/src/test/Provider.java
@@ -1,5 +1,8 @@
 public class Provider {
     public static void main(String[] args) {
+
         com.alibaba.dubbo.container.Main.main(args);
+
+
     }
 }
--- platform-front/src/main/java/com/frog/platform/controller/UserController.java
+++ platform-front/src/main/java/com/frog/platform/controller/UserController.java
@@ -2,6 +2,7 @@ package com.frog.platform.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -9,10 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @RequestMapping("register.do")
-    public String register(){
+    public String register(ModelMap map){
+
+//        map.put("backgroundUrl","/images/Europe.jpg");//日后拓展
 
 
         return "global/register";
+    }
+    @RequestMapping("login.do")
+    public String login(ModelMap map){
+
+//        map.put("backgroundUrl","/images/Europe.jpg");//日后拓展
+
+
+        return "global/login";
     }
 
 }
